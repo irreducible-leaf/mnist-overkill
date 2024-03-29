b800bca5a3ce6cd46eee4c027b336170cdb317cc
irreducible-leaf <xavierdacruz1@googlemail.com>
2024-03-29 17:01:13 +0000

    rearranged project structure and improved model

--- mnist_overkill/deployment/gradio_app.py
+++ mnist_overkill/deployment/gradio_app.py
@@ -1,27 +1,18 @@
 import os
 import gradio as gr
 import torch
-import torch.nn as nn
 import numpy as np
 from PIL import Image
+from small_mlp import SmallMLP
 
-# Define the logistic regression model class
-class LogisticRegression(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(LogisticRegression, self).__init__()
-        self.linear = nn.Linear(input_size, num_classes)
-
-    def forward(self, x):
-        out = self.linear(x)
-        return out
 
 # Define the function to load the model
 def load_model(model_path):
     # Load the saved model
     checkpoint = torch.load(model_path)
 
-    # Reinitialize the model with the saved input size and number of classes
-    model = LogisticRegression(checkpoint['input_size'], checkpoint['num_classes'])
+    # Load the model from the checkpoint
+    model = checkpoint['model_architecture']
 
     # Load the model's state dictionary
     model.load_state_dict(checkpoint['model_state_dict'])
@@ -37,6 +28,7 @@ def predict_image(image):
     # Preprocess the image (convert to grayscale and normalize)
     image = image.mean(axis=2)  # Convert to grayscale
     image = image / 255.0  # Normalize
+    image = image.flatten()
 
     # Preprocess the image (reshape and convert to tensor)
     image = np.expand_dims(image, axis=0)  # Add batch dimension
@@ -45,18 +37,16 @@ def predict_image(image):
     # Make prediction
     model.eval()
     with torch.no_grad():
-        output = model(image_tensor.view(-1, 28*28))
+        output = model(image_tensor)
         _, predicted = torch.max(output, 1)
         prediction = predicted.item()  # Get the predicted class index
 
     return str(prediction)
+
 # Load the trained model
-model_path = os.path.join('models', 'logistic_regression_model.pth')  # Adjust the path as needed
+model_path = os.path.join('small_mlp_model.pth')
 model = load_model(model_path)
 
-# # Create Gradio interface
-# image = gr.inputs.Image(shape=(28, 28), image_mode='L', source="canvas")  # image_mode='L' for grayscale
-# label = gr.outputs.Label(num_top_classes=1)
-
-demo = gr.Interface(fn=predict_image, inputs='image', outputs='label', title="Simple Image Classifier", description="Upload an image or draw on the canvas to classify.")
+# Create Gradio interface
+demo = gr.Interface(fn=predict_image, inputs='image', outputs='label', title="Simple MNIST classifier", description="Upload an image or draw on the canvas to classify.")
 demo.launch(server_port=8080, server_name="0.0.0.0")
---
+++ mnist_overkill/small_mlp.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SmallMLP(nn.Module):
+    def __init__(self, input_size, hidden_size1, hidden_size2, num_classes):
+        super(SmallMLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.fc3 = nn.Linear(hidden_size2, num_classes)
+
+    def forward(self, x):
+        # x = x.view(-1, 28 * 28)  # Flatten the input
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
